a67fa3bf4407364b9be61364a3f80ca9a5efcd71
Tia King <tiajaneeking@gmail.com>
2018-01-15 03:09:36 -0800

    Added Comments

--- PagePlaner/TimerViewController.swift
+++ PagePlaner/TimerViewController.swift
@@ -8,27 +8,31 @@
 
 import UIKit
 
-
-
 class TimerViewController: UIViewController {
     
     @IBOutlet weak var timerLabel: UILabel!
+    
+    //initial time starts at 0
     var time = 0.00
     //timer
     var timer = Timer()
     
     @IBAction func resetButton(_ sender: Any) {
+        //stops timer and resets it back to 0
         timer.invalidate()
         time = 0.00
         timerLabel.text = "0.00"
     }
     @IBAction func endTimer(_ sender: UIButton) {
+        //stops timer
         timer.invalidate()
     }
     @IBAction func startTimer(_ sender: Any) {
+        //timer with milliseconds
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(TimerViewController.action), userInfo: nil, repeats: true)
     }
     @objc func action() {
+        //formatting timers
         time += 0.01
         timerLabel.text = String(format: "%3.2f", time)
     }
