7d81fe039ef7b3c60d6ff499c6fb22933bfee9fe
Tia King <tiajaneeking@gmail.com>
2018-02-08 01:41:09 -0800

    Customized v1 of Library of Readings

---
+++ PagePlaner/ReadingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReadingsTableViewCell.swift
+//  PagePlaner
+//
+//  Created by Tia King on 2/8/18.
+//  Copyright © 2018 Tia King. All rights reserved.
+//
+
+import UIKit
+
+class ReadingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
