c3520d98fef91fcda0990cd3204adbf75cfe3b34
Tia King <tiajaneeking@gmail.com>
2018-02-23 16:55:00 -0800

    View Controller for Library

--- PagePlaner/LibraryTableViewController.swift
+++ PagePlaner/LibraryTableViewController.swift
@@ -7,10 +7,16 @@
 //
 
 import UIKit
+import CoreData
+
+public final class MangagedSingleton {
+    static let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+}
 
 class LibraryTableViewController: UITableViewController {
-    var readings = ["Where The Wild Things Are", "15 Days",
-                    "Grandma Got Runover By A Reindeer"]
+    
+    var readings = [Reading]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,15 +25,19 @@ class LibraryTableViewController: UITableViewController {
     self.navigationController?.navigationBar.titleTextAttributes = [ NSAttributedStringKey.font: UIFont(name: "LibreBarcode39Text-Regular", size: 40)!]
         
     self.navigationController?.navigationBar.barTintColor = UIColor.white
-
-       
-
-       
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        loadData()
+    }
+    
+    func loadData() {
+        let readingRequest:NSFetchRequest<Reading> = Reading.fetchRequest()
+        
+        do {
+            readings = try MangagedSingleton.managedObjectContext.fetch(readingRequest)
+            self.tableView.reloadData()
+        } catch {
+            print("oh no nigga")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,16 +45,12 @@ class LibraryTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 8
+        return readings.count
     }
 
     
@@ -52,13 +58,42 @@ class LibraryTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LibCell", for: indexPath) as! ReadingsTableViewCell
 
         // Configure the cell...
+        
+        let readingItem = readings[indexPath.row]
+        cell.calcDate.text = readingItem.dueDate
+        cell.calcName.text = readingItem.name
+        cell.calcPace.text = readingItem.pace
 
         return cell
     }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 110
     }
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+//            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+//            let readingRequest:NSFetchRequest<Reading> = Reading.fetchRequest()
+//            if let result = try? MangagedSingleton.managedObjectContext.fetch(readingRequest) {
+//                for object in result {
+//                    MangagedSingleton.managedObjectContext.delete(object)
+//                    try MangagedSingleton.managedObjectContext.save()
+//                    catch do {
+//                        print("could not delete")
+//                    }
+//                }
+//            }
+//            //delete reading from Core Data
+            print("delete tableview cell")
+            tableView.reloadData()
 
+
+        }
+    }
    
     
 
